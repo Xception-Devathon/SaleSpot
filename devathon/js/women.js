384bc3726ff62f28f65fcc5cde704ea2060c8c9d
saransh-283 <saranshmm@gmail.com>
2023-04-06 15:49:30 +0530

    Women Page

--- devathon/js/women.js
+++ devathon/js/women.js
@@ -1,9 +1,9 @@
 import Card from "./card.js";
 
-fetch("https://dummyjson.com/products/category/mens-shirts?limit=10")
+fetch("https://dummyjson.com/products/category/tops?limit=10")
   .then((response) => response.json())
   .then((data) => {
-    const picksElement = document.getElementById("shirts");
+    const picksElement = document.getElementById("tops");
     data.products.forEach((productData) => {
       const product = Card(
         productData.thumbnail,
@@ -16,7 +16,23 @@ fetch("https://dummyjson.com/products/category/mens-shirts?limit=10")
   })
   .catch((error) => console.error(error));
 
-fetch("https://dummyjson.com/products/category/mens-shoes?limit=10")
+fetch("https://dummyjson.com/products/category/womens-dresses?limit=10")
+  .then((response) => response.json())
+  .then((data) => {
+    const picksElement = document.getElementById("dresses");
+    data.products.forEach((productData) => {
+      const product = Card(
+        productData.thumbnail,
+        productData.title,
+        productData.price,
+        productData.rating
+      );
+      picksElement.appendChild(product);
+    });
+  })
+  .catch((error) => console.error(error));
+
+fetch("https://dummyjson.com/products/category/womens-shoes?limit=10")
   .then((response) => response.json())
   .then((data) => {
     const picksElement = document.getElementById("shoes");
@@ -32,7 +48,7 @@ fetch("https://dummyjson.com/products/category/mens-shoes?limit=10")
   })
   .catch((error) => console.error(error));
 
-fetch("https://dummyjson.com/products/category/mens-watches?limit=10")
+fetch("https://dummyjson.com/products/category/womens-watches?limit=10")
   .then((response) => response.json())
   .then((data) => {
     const picksElement = document.getElementById("watches");
@@ -47,11 +63,26 @@ fetch("https://dummyjson.com/products/category/mens-watches?limit=10")
     });
   })
   .catch((error) => console.error(error));
-
-fetch("https://dummyjson.com/products/category/sunglasses?limit=10")
+fetch("https://dummyjson.com/products/category/womens-bags?limit=10")
   .then((response) => response.json())
   .then((data) => {
-    const picksElement = document.getElementById("sunglasses");
+    const picksElement = document.getElementById("bags");
+    data.products.forEach((productData) => {
+      const product = Card(
+        productData.thumbnail,
+        productData.title,
+        productData.price,
+        productData.rating
+      );
+      picksElement.appendChild(product);
+    });
+  })
+  .catch((error) => console.error(error));
+
+fetch("https://dummyjson.com/products/category/womens-jewellery?limit=10")
+  .then((response) => response.json())
+  .then((data) => {
+    const picksElement = document.getElementById("jewellery");
     data.products.forEach((productData) => {
       const product = Card(
         productData.thumbnail,
