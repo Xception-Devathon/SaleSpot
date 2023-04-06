d3e18d564633c027cdd831cec147e83618f48743
saransh-283 <saranshmm@gmail.com>
2023-04-06 16:20:53 +0530

    Random top picks

--- devathon/js/home.js
+++ devathon/js/home.js
@@ -1,17 +1,20 @@
 import Card from "./card.js";
 
-fetch('https://dummyjson.com/products?limit=10')
-  .then(response => response.json())
-  .then(data => {
-    const picksElement = document.getElementById('picks');
-    data.products.forEach(productData => {
-      const product = Card(
-        productData.thumbnail,
-        productData.title,
-        productData.price,
-        productData.rating
-      );
-      picksElement.appendChild(product);
-    });
+fetch("https://dummyjson.com/products?limit=0")
+  .then((response) => response.json())
+  .then((data) => {
+    const picksElement = document.getElementById("picks");
+    data.products
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 10)
+      .forEach((productData) => {
+        const product = Card(
+          productData.thumbnail,
+          productData.title,
+          productData.price,
+          productData.rating
+        );
+        picksElement.appendChild(product);
+      });
   })
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
